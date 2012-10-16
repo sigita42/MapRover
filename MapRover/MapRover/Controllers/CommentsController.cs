c66c3f9d6a298f06a80eb5a14c10ae8bdd96adb5
sigita grinfelde <sigita.grinfelde@gmail.com>
2012-10-17 02:07:30 +0300

    Controllers

---
+++ MapRover/MapRover/Controllers/CommentsController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MapRover.Models.ViewModels;
+
+namespace MapRover.Controllers
+{
+    public class CommentsController : Controller
+    {
+        //
+        // GET: /Comments/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //
+        // GET: /Map/Index/
+        // Activates by pressing button - Go back to map
+
+        public ActionResult Map()
+        {
+            return View();
+        }
+        // No idea what Im doing here - should tell controller to take info about City details from Map Controller. Later must be edited.
+        public ActionResult CityDetails()
+        {
+            return View();
+        }
+        }
+ }
+    
+
---
+++ MapRover/MapRover/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MapRover.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to kick-start your ASP.NET MVC application.";
+
+            return View();
+        }
+        //
+        // GET: /Home/About/
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "App description page.";
+
+            return View();
+        }
+        //
+        // GET : /Home/Contact/
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact page.";
+
+            return View();
+        }
+    }
+}
